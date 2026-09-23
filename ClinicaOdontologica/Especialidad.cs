52087081453a600a14ff1afab60217b1bc5f9b66
Kevin Chávez <200877875+VendrichDev@users.noreply.github.com>
2026-09-23 11:10:10 -0500

    Agregar archivos de proyecto.

---
+++ ClinicaOdontologica/Especialidad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaOdontologica
+{
+    [Table("especialidades")]
+    public class Especialidad
+    {
+
+        [Key]
+        [Column("id_especialidad")]
+        public int idEspecialidad { get; set; }
+
+
+        [Column("descripcion"), MaxLength(200), Required]
+        public string Descripcion { get; set; }
+
+
+        [Column("nombre_especialidad"), MaxLength(50), Required]
+        public string NombreEspecialidad { get; set; }
+    }
+}
